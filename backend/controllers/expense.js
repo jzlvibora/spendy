266834dd4d661f2be9eb3949182af902fc0b6819
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2025-02-07 17:28:55 +0800

    added base BE crud features

---
+++ backend/controllers/expense.js
@@ -0,0 +1,45 @@
+const ExpenseSchema = require("../models/ExpenseModel");
+
+exports.addExpense = async (req, res) => {
+  const { title, category, description, amount, date } = req.body;
+  const expense = ExpenseSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+  try {
+    if (!title || !category || !description || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (amount <= 0 || typeof amount !== "number") {
+      return res
+        .status(400)
+        .json({ message: "Amount should be a positive number" });
+    }
+    await expense.save();
+    return res.status(200).json({ message: "Expense added" });
+  } catch (err) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getExpenses = async (req,res) => {
+    try{
+        const expenses = await ExpenseSchema.find().sort({createdAt:-1})
+        res.status(200).json(expenses)
+    }
+    catch(error){
+        res.status(500).json({message: "Server error"})
+    }
+}
+
+exports.deleteExpense = async (req,res) => {
+    const {id} = req.params;
+    ExpenseSchema.findByIdAndDelete(id).then((expense)=> {
+        res.status(200).json({message:"Expense deleted"})
+    }).catch((error)=>{
+        res.status(500).json({message:"Server error"})
+    })
+}
---
+++ backend/controllers/income.js
@@ -0,0 +1,46 @@
+const IncomeSchema = require("../models/IncomeModel");
+
+exports.addIncome = async (req, res) => {
+  console.log(req.body);
+  const { title, category, description, amount, date } = req.body;
+  const income = IncomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+  try {
+    if (!title || !category || !description || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (amount <= 0 || typeof amount !== "number") {
+      return res
+        .status(400)
+        .json({ message: "Amount should be a positive number" });
+    }
+    await income.save();
+    return res.status(200).json({ message: "Income added" });
+  } catch (err) {
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getIncomes = async (req,res) => {
+    try{
+        const incomes = await IncomeSchema.find().sort({createdAt:-1})
+        res.status(200).json(incomes)
+    }
+    catch(error){
+        res.status(500).json({message: "Server error"})
+    }
+}
+
+exports.deleteIncome = async (req,res) => {
+    const {id} = req.params;
+    IncomeSchema.findByIdAndDelete(id).then((income)=> {
+        res.status(200).json({message:"Income deleted"})
+    }).catch((error)=>{
+        res.status(500).json({message:"Server error"})
+    })
+}
